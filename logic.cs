ccfa5b773de74862eeabc65897a1eebdff889782
Michael Evanchik <MichaelEvanchik@users.noreply.github.com>
2016-02-11 14:18:50 -0500

    Update logic.cs

--- logic.cs
+++ logic.cs
@@ -16,54 +16,20 @@ namespace shipstation_erp
 
 protected void SomeEventYourIn()
 {
-                                        util u = new util();
-                                        oShipStationCreateOrderRequest c = new oShipStationCreateOrderRequest();
-                                        c = u.GetShipStationCreateOrderRequestByExternalRef(external_ref);
+                            var resp = new MyWebResponse();
+                            resp = u.PostJsonShipStation((oShipStationCreateLabelRequest)ol, "https://ssapi.shipstation.com/orders/createlabelfororder");
+                            
 
-                                        HttpWebResponse wresp = null;
-                                        wresp = u.PostJsonCreateOrderShipStation(c);
-                                        var encoding = ASCIIEncoding.ASCII;
-                                        responseText = "";
+                            if (resp.bexception == false)
+                            {
+                                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                                var oRes = new oShippmentCreateLabelResponse();
 
-                                        using (var reader = new System.IO.StreamReader(wresp.GetResponseStream(), encoding))
-                                        {
-                                                responseText = reader.ReadToEnd();
-                                        }
-
-                                        JavaScriptSerializer serializer = new JavaScriptSerializer();
-                                        var oRes = new oShippmentCreateOrderResponse();
-
-                                        oRes = serializer.Deserialize<oShippmentCreateOrderResponse>(responseText);
-
-                                        long? orderId;
-                                        orderId = oRes.orderId;
-
-                                        if (orderId > 0)
-                                        {
-                                            var encoding2 = ASCIIEncoding.ASCII;
-                                            oShipStationCreateLabelRequest ol = new oShipStationCreateLabelRequest();
-                                            ol = u.GetShipStationCreateLabelRequestByExternalRef(external_ref, orderId);
-                                            responseText = "";
-                                            HttpWebResponse wresp2 = null;
-                                            wresp2 = u.PostJsonPrintLabelShipStation(ol);
-
-                                            using (var reader = new System.IO.StreamReader(wresp2.GetResponseStream(), encoding2))
-                                            {
-                                                responseText = reader.ReadToEnd();
-                                            }
-                                            JavaScriptSerializer serializer2 = new JavaScriptSerializer();
-                                            var oRes2 = new oShippmentCreateLabelResponse();
-
-                                            oRes2 = serializer2.Deserialize<oShippmentCreateLabelResponse>(responseText);
-                                            var binary = System.Convert.FromBase64String(oRes2.labelData);
-                                            string spath = "c:\\inetpub\\wwwroot\\ftproot\\shipstation\\work_orders\\" + lsale_date.Year.ToString() + "\\" + lsale_date.Month.ToString() + "\\" + lsale_date.Day.ToString() + "\\";
-                                            System.IO.Directory.CreateDirectory(spath + external_ref.ToString());
-
-                                            File.WriteAllBytes(spath + external_ref.ToString() + "\\Label" + external_ref.ToString() + ".pdf", binary);
-                                            using (var dc = new stylusDataContext())
-                                            {
-                                                dc.sp_update_order_and_items_tracking(external_ref, oRes2.trackingNumber, null, oRes2.insuranceCost, orderId.ToString(), null, DateTime.Now, oRes2.shipmentCost, oRes2.shipmentId);
-                                            }
-                                        }
-}                                        
+                                oRes = serializer.Deserialize<oShippmentCreateLabelResponse>(r.smessage);
+                                var binary = System.Convert.FromBase64String(oRes.labelData);
+                                spath += lsale_date.Year.ToString() + "\\" + lsale_date.Month.ToString() + "\\" + lsale_date.Day.ToString() + "\\";
+                                System.IO.Directory.CreateDirectory(spath + external_ref.ToString());
+                                File.WriteAllBytes(spath + external_ref.ToString() + "\\Label" + external_ref.ToString() + ".pdf", binary);
+                            }
 }
+                        
